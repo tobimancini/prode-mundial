132b789824c60b56e9e6626cd1da33941f5d1e0a
Tobias Mancini <tobias_mancini@hotmail.com>
2022-11-11 14:13:08 -0300

    borrar prediccion user

--- src/Components/Utils/borrarJugador.js
+++ src/Components/Utils/borrarJugador.js
@@ -1,8 +1,19 @@
-import { deleteDoc, doc } from "firebase/firestore";
+import { collection, deleteDoc, doc, getDocs, query, where } from "firebase/firestore";
 import { db } from "../../Firebase/config";
 
-const borrarJugador = async(id) =>{
+const borrarJugador = async(id, uid) =>{
     await deleteDoc(doc(db, "Usuarios", id));
+
+    const q = query(collection(db, "Predicciones"), where("uid", "==", uid));
+
+    const querySnapshot = await getDocs(q);
+    
+    querySnapshot.forEach(async (doc) => {
+        let predId = doc.id;  
+        await deleteDoc(doc(db, "Predicciones", predId));
+    })
+
+
 }
 
 export default borrarJugador;
