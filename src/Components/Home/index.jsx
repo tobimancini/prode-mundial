73869433abeba637a7d98fb7cba89f3aa28108e1
Tobias Mancini <tobias_mancini@hotmail.com>
2022-11-14 00:26:25 -0300

    titulo

--- src/Components/Home/index.jsx
+++ src/Components/Home/index.jsx
@@ -5,7 +5,7 @@ const Home = () => {
     return (
         <div className='homeContainer'>
             <div className='logoTitle'>
-                <h1>PRODE QATAR</h1>
+            <img src={process.env.PUBLIC_URL + "/images/prodeTitulo.png"} alt="titulo" className='prodeTitulo'/>
             </div>
             <img src={process.env.PUBLIC_URL + "/images/cruyff.jpg"} alt="maradona" className='maradona'/>
             <img src={process.env.PUBLIC_URL + "/images/logoJaula.png"} alt="logoJaula" className='logoJaula'/>
