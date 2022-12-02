40f97480a4a5df26687d853e5dd7ffa1db50fc23
Tobias Mancini <tobias_mancini@hotmail.com>
2022-12-02 18:49:06 -0300

    octavos completo

--- src/Components/Utils/sinPredic.js
+++ src/Components/Utils/sinPredic.js
@@ -10,7 +10,7 @@ const sinPredic = async () => {
     // querySnapshot.forEach((doc) => {
     //     habilitados.push(doc.data())
     // });
-    const q = query(collection(db, "Predicciones"), where("apellido", "==", "PICCARDO"), where("nombre", "==", "SANTOS"));
+    const q = query(collection(db, "Predicciones"), where("apellido", "==", "GRAVINA"));
 
     const querySnapshot = await getDocs(q);
     querySnapshot.forEach((doc) => {
